0db819f63fc3063a841d9112d012fa5bb553dd11
AgustinFrich <agustinfrich@gmail.com>
2024-04-18 10:55:04 -0300

    firts commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ErrorComponent } from './components/error/error.component';
+import BienvenidaComponent from './components/bienvenida/bienvenida.component';
+
+export const routes: Routes = [
+  {
+    path: 'bienvenida', // ¿estoy en '/bienvenida'?
+    loadComponent: () => import('./components/bienvenida/bienvenida.component'),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login.component'),
+  },
+  {
+    path: 'community',
+    loadComponent: () =>
+      import('./components/community/community.component').then(
+        (m) => m.CommunityComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () => import('./components/bienvenida/bienvenida.component'),
+  },
+  {
+    path: 'bienvenido',
+    redirectTo: 'bienvenida',
+  },
+  {
+    path: '**', // ¿estoy en 'CUALQUIER RUTA'?
+    component: ErrorComponent,
+  },
+];
---
+++ src/app/components/bienvenida/bienvenida.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bienvenida',
+  standalone: true,
+  imports: [],
+  templateUrl: './bienvenida.component.html',
+  styleUrl: './bienvenida.component.scss',
+})
+export default class BienvenidaComponent {}
